2019b49a0ccf057e548cb650e66477db60a540e3
Sourav Dutta <sourav.equilizer007@gmail.com>
2022-11-25 21:08:41 +0530

    Adding recipe.module & recipe-routing.module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,36 +6,22 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingService } from './shopping-list/shopping.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
-import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptor } from './auth/auth-interceptor.service';
 import { AppAlertComponent } from './shared/alert/app-alert/app-alert.component';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailsComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AppAlertComponent
@@ -46,6 +32,7 @@ import { AppAlertComponent } from './shared/alert/app-alert/app-alert.component'
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    RecipesModule
   ],
   providers: [ShoppingService, RecipeService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from "@angular/core";
+
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+import { DropdownDirective } from "../shared/dropdown.directive";
+
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailsComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent,
+        DropdownDirective
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule
+    ],
+    /*  We are not exporting the recipe components here because we are not explicitly using these components anywhere in the app or in any child component of the app apart from this internal recipe folder.
+    And so there is no need to export the components in this case! */
+    // exports: [
+    //     RecipesComponent,
+    //     RecipeListComponent,
+    //     RecipeDetailsComponent,
+    //     RecipeItemComponent,
+    //     RecipeStartComponent,
+    //     RecipeEditComponent,
+    //     DropdownDirective
+    // ]
+})
+export class RecipesModule {
+}
