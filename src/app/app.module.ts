c0bcfec091187ef3019e03b47b20f1db331f849f
Sourav Dutta <sourav.equilizer007@gmail.com>
2022-11-25 21:44:02 +0530

    Adding shopping-list.module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { ShoppingService } from './shopping-list/shopping.service';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
@@ -15,16 +13,17 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
 import { AuthInterceptor } from './auth/auth-interceptor.service';
 import { AppAlertComponent } from './shared/alert/app-alert/app-alert.component';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
-    AppAlertComponent
+    AppAlertComponent,
+    // DropdownDirective
   ],
   imports: [
     BrowserModule,
@@ -32,7 +31,8 @@ import { RecipesModule } from './recipes/recipes.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [ShoppingService, RecipeService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
