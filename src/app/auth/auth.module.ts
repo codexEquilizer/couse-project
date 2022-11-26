edd4f6abc73ac18b24451b119450df2832599beb
Sourav Dutta <sourav.equilizer007@gmail.com>
2022-11-26 18:08:31 +0530

    Adding an auth Feature Module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { AuthComponent } from "./auth.component";
+
+const routes: Routes = [
+    { path: 'auth', component: AuthComponent }
+]
+
+@NgModule({
+    declarations: [AuthComponent],
+    imports: [
+        ReactiveFormsModule,
+        RouterModule.forChild(routes),
+        SharedModule        // SharedModule contains CommonModule so need to import CommonModule separately
+    ]
+})
+export class AuthModule { }
