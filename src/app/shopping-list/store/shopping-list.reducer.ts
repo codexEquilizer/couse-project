9ccd2605e7564b67df8839176f2871f899153bfb
Sourav Dutta <sourav.equilizer007@gmail.com>
2022-11-29 14:21:43 +0530

    Setting up ngrx store

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,5 @@
-import { Action } from "@ngrx/store";
 import { Ingredient } from "../../shared/ingredient.model";
-import { ADD_INGREDIENT } from "./shopping-list.action";
+import * as  ShoppingListActions from "./shopping-list.action";
 
 /* initial state  (Note state should be a JS object)*/
 const initialState = {
@@ -12,14 +11,14 @@ const initialState = {
 
 /* Reducer */
 // only for the first time when ngrx will initialize the state then it will be our initial state. So in the parameter I have written state=initialState such that if the value of state in null then it will take the initialState value by default
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
     switch (action.type) {
-        case ADD_INGREDIENT:
+        case ShoppingListActions.ADD_INGREDIENT:
             //need to return a new state
             //NOTE: State changes with ngrx will always have to be immutable. So we do not touch the current/existing state
             return {
                 ...state,   // copy of the current state
-                ingredients: [...state.ingredients, action]
+                ingredients: [...state.ingredients, action.payload]
             }
     }
 
